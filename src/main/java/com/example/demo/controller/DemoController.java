bbfe5643ae78f88daeda6c2d95dc031fee9db6ff
luca-dgdev <luca.dgdev@gmail.com>
2019-02-17 13:26:06 +0100

    add endpoint hello

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public ResponseEntity<String> hello() {
+		return new ResponseEntity<String>("Hello", HttpStatus.OK);
+	}
+
+}

805b3f55c78e9ef575d5c18875282a5117f93ae9
luca-dgdev <luca.dgdev@gmail.com>
2019-02-23 14:39:53 +0100

    Add logging messages

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -8,17 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.DemoApplication;
-
 @RestController
 @RequestMapping("/api")
 public class DemoController {
-	
+
 	private static final Logger log = Logger.getLogger(DemoController.class.getName());
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public ResponseEntity<String> hello() {
-		log.info("HELLO");
+		log.severe("HELLO");
 		return new ResponseEntity<String>("Hello", HttpStatus.OK);
 	}
 
